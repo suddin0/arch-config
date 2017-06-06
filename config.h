ec2f5fd6b01ec72e893a7685358bb3d6e929825a
suddin0 <suddin@student.42.fr>
2017-06-07 01:44:19 +0200

    mostly done

---
+++ config.h
@@ -0,0 +1,35 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Description */
+#define GOT_LIBXFCEUI /**/
+
+/* Name of package */
+#define PACKAGE "Xfce-Theme-Manager"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "PROJ"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Xfce-Theme-Manager"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Xfce-Theme-Manager 0.3.7"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "Xfce-Theme-Manager"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.3.7"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.3.7"
+
+/* xfdesktop 4.11.1 */
+#define _411_ /**/
