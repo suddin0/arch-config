a5e76d65e1d22dfd03c4e36c58cf266b3899a851
suddin0 <suddin@student.42.fr>
2017-08-02 02:25:24 +0200

    fish conf file

---
+++ config.fish
@@ -0,0 +1,17 @@
+alias l="ls"
+#aloas to check leaks
+alias leaks="valgrind --leak-check=yes --track-origins=yes"
+
+#code directory alias
+alias code="cd ~/Bureau/__code__/"
+alias codec="cd ~/Bureau/__code__/__C__/"
+alias codecy="cd ~/Bureau/__code__/__C__/__YAK__"
+alias codecsock="cd ~/Bureau/__code__/__C__/__SOCKET__/"
+
+#git alias
+alias gcln="git clone"
+alias gstat="git status"
+alias gadd="git add"
+alias gcom="git commit -m"
+alias gph="git push"
+
